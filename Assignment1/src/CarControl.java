0f0ddef2e6a6b80ad9a2f41af384b64a4f8f87c2
Frederik Kirkegaard <frederik-kirkegaard@hotmail.com>
2018-10-25 17:06:38 +0200

    Fixed alley sync

--- Assignment1/src/CarControl.java
+++ Assignment1/src/CarControl.java
@@ -135,7 +135,7 @@ class Conductor extends Thread {
 
 
 				if(inCriticalRegion == true && criticalRegionExits.contains(newpos) && criticalRegionEntrances.contains(curpos)) {
-					_alley.leave();
+					_alley.leave(no);
 					inCriticalRegion = false;
 				}
 
