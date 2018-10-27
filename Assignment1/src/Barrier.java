d6868f3c5beb8ac529596c83884416d5cdeeef27
Mathias <mathiasbj94@gmail.com>
2018-10-27 17:23:48 +0200

    Stop and start of barrier implemented, still buggy for on/off

--- Assignment1/src/Barrier.java
+++ Assignment1/src/Barrier.java
@@ -1,25 +1,30 @@
 public class Barrier {
 
     static boolean barrierOn = false;
-    static Semaphore barrierSemaphores[];
+    static Semaphore[] barrierSemaphores;
 
     public Barrier(){
+        this.barrierSemaphores = new Semaphore[9];
+        for (int i = 0; i < barrierSemaphores.length; i++){
+            barrierSemaphores[i] = new Semaphore(0);
+        }
 
 
     }
 
     public static void sync(int no){
         // Add a semaphore to every car's barrier semaphore except its own.
-        for (int j = 0; j < 10 ; j++){
+        for (int j = 0; j < 9 ; j++){
             if (j != no){
                 barrierSemaphores[j].V();
-                System.out.println(barrierSemaphores[j]);
+                System.out.println("sync entered");
             }
         }
         try {
         // Waits until it can pick up all 8 semaphores before allowed passing the barrier.
-        for (int i = 0; i < 9 ; i++){
+        for (int i = 0; i < 8 ; i++){
             barrierSemaphores[no].P();
+            System.out.println("Car number " + no + " picked up " + i );
         }
 
         } catch (InterruptedException e) {
