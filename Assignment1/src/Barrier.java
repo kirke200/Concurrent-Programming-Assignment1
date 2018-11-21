ae77d0d146cd19cbdc833c817ee00dc3a4ccbf42
Mathias <mathiasbj94@gmail.com>
2018-11-21 16:36:18 +0100

    Fixed cars getting stuck after turning barrier on and off at -1 speeds

--- Assignment1/src/Barrier.java
+++ Assignment1/src/Barrier.java
@@ -18,37 +18,38 @@ public class Barrier {
         K = 0;
 
     }
-    // with inspiration from barrier monitor (Synchronization mechanisms).
     public synchronized void sync() {
-        while (OK)
+        if(!barrierOn){
+            return;
+        }
+        while (OK && barrierOn)
             try {wait();} catch (InterruptedException e) {}
         K++;
-        if (K == ncars) { // If all cars are waiting at the barrier.
+        if (K == ncars && barrierOn) { // If all cars are waiting at the barrier.
             OK = true;
             notifyAll();
         }
-        while (!OK)
+        while (!OK && barrierOn)
             try {wait();} catch (InterruptedException e) {}
         K--;
-        if (K == 0) {
+        if (K == 0 && barrierOn) {  // If all cars have left, allow cars waiting to wait for monitor to enter the critical section
             OK = false;
             notifyAll();
         }
     }
-
+    // Turns on barrier
     public synchronized void on(){
-        barrierOn = true;
         OK = false;
+        barrierOn = true;
 
     }
-
+    // Turns off barrier
     public synchronized void off(){
-        System.out.println("Barrier off!");
         barrierOn = false;
         OK = true;
         notifyAll();
     }
-
+    // Checks if a car should stop at the barrier.
     public synchronized void isPosBarrierEntrance(Pos pos){
         if (barrierEntrance.contains(pos) && barrierOn==true){
             sync();
@@ -57,80 +58,3 @@ public class Barrier {
         }
     }
 }
-
-
-
-/*
-// Semaphore Solution
-
-import java.util.ArrayList;
-        import java.util.Arrays;
-
-public class Barrier {
-
-    static boolean barrierOn = false;
-    static Semaphore[] barrierSemaphores = new Semaphore[9];
-    ArrayList<Pos> barrierEntrance = new ArrayList<>(Arrays.asList(new Pos(4,3), new Pos(4,4), new Pos(4,5), new Pos(4,6), new Pos(4,7)
-            , new Pos(5,8), new Pos(5,9), new Pos(5,10), new Pos(5,11)));
-
-    public Barrier(){
-
-    }
-
-    public void sync(int no){
-        // Add a semaphore to every car's barrier semaphore except its own.
-        for (int j = 0; j < 9 ; j++){
-            if (j != no){
-                barrierSemaphores[j].V();
-            }
-        }
-        try {
-            // Waits until it can pick up all 8 semaphores before allowed passing the barrier.
-            for (int i = 0; i < 8 ; i++){
-                barrierSemaphores[no].P();
-                System.out.println("Car number " + no + " picked up " + i );
-            }
-
-        } catch (InterruptedException e) {
-        }
-
-    }
-
-    public void on(){
-        // Resets the semaphores to 0 for every index
-        // ER MÅSKE DUMT AT LAVE NYE SEMAPHORE OBJECTER? MEN ER DET BEDRE AT RESETTE DEM MED .P()?
-        for (int i = 0; i < barrierSemaphores.length; i++){
-            barrierSemaphores[i] = new Semaphore(0);
-        }
-
-        barrierOn = true;
-
-    }
-
-    public void off(){
-        System.out.println("BARRIER OFF");
-        barrierOn = false;
-        // Puts 9 semaphores into every index of the array to allow waiting cars to proceed.
-        for (int j = 0; j < 9 ; j++) {
-            for (int i = 0; i < 9; i++) {
-                barrierSemaphores[j].V();
-            }
-        }
-        System.out.println(String.valueOf(barrierOn));
-        //for ( sem in )
-
-
-    }
-    public void isPosBarrierEntrance(Pos pos, int no){
-        if (barrierEntrance.contains(pos) && barrierOn==true){
-            sync(no);
-
-
-        }
-    }
-}
-
-
-
-
-*/
